00bceaa765ce31b7920624917cfad870aa1dca9c
tcdw <admin@tcdw.net>
2017-08-01 14:22:46 +0900

    补上了原主题信息

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 ![hexo-theme-apollo](https://cloud.githubusercontent.com/assets/9530963/13026956/08e76eca-d277-11e5-8bfc-2e80cea20a0d.png)
 
+[原主题（Hexo 版）](https://github.com/pinggod/hexo-theme-apollo)
+
 ## 安装
 
 ``` bash
--- install.sh
+++ install.sh
@@ -6,8 +6,8 @@ fi
 if [ ! -d "Apollo" ]; then
     git clone https://github.com/SilverBlogTeam/Apollo.git
     cd Apollo
+    ln -s $(pwd)/static/Apollo ../static/Apollo
 fi
-ln -s $(pwd)/static ../static/Apollo
 if [ -f "config.json" ]; then
     cp config.example.json config.json
     vim config.json
