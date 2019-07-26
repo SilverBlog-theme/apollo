adba6f7455e264e68b9a148edd801d0bbe541aab
tcdw <tcdw2011@gmail.com>
2019-07-27 00:24:36 +0800

    不再使用 timeSince

--- static/js/apollo.js
+++ static/js/apollo.js
@@ -1,27 +1,3 @@
-var gapType = [
-    {max: 60, suffix: " seconds ago"},
-    {max: 3600, suffix: " minutes ago"},
-    {max: 86400, suffix: " hours ago"},
-    {max: 604800, suffix: " days ago"},
-    {max: 2592000, suffix: " weeks ago"},
-    {max: 31536000, suffix: " months ago"}
-];
-
-timeSince = function (date) {
-    var gap = (new Date().getTime() - date) / 1000;
-    var i = 0;
-    while (true) {
-        if (gap >= ((i <= 0) ? 0 : gapType[i - 1].max) && gap < gapType[i].max) {
-            return Math.floor(gap / ((i <= 0) ? 1 : gapType[i - 1].max)) + gapType[i].suffix;
-        } else if (gap > gapType[gapType.length - 1].max || gap < 0 || i >= gapType.length) {
-            var ds = new Date(date).toDateString().split(" ");
-            return ds[1] + " " + ds[2] + ", " + ds[3];
-        } else {
-            i += 1;
-        }
-    }
-};
-
 var uri = new URL(location.href);
 var nav_div = document.getElementById("nav-list");
 var links = nav_div.getElementsByTagName("a");
@@ -36,12 +12,3 @@ if(url_path!==""){
     }
 }
 links[index].className = 'nav-list-link active';
-
-var dateItem = document.getElementsByClassName("post-info");
-for (var i in dateItem) {
-    if (dateItem[i].innerHTML != "") {
-        dateItem[i].innerHTML = timeSince(new Date(dateItem[i].innerHTML).getTime());
-    }
-}
-
-hljs.initHighlightingOnLoad();
